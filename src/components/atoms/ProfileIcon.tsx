224e2df902acc13b616a5a8748b91ffa78271551
dak2 <dawt2h@gmail.com>
2020-11-22 19:02:00 +0900

    プロフィールアイコンの作成

---
+++ src/components/atoms/ProfileIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+export default function ProfileIcon() {
+  return (
+    <AccountCircleIcon />
+  )
+}
