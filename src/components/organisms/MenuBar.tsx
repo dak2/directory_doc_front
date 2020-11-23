af7440032b599ffd7c684b4b766d5a834ed11ad0
dak2 <dawt2h@gmail.com>
2020-11-23 14:30:48 +0900

    Textコンポーネント作成

--- src/components/organisms/MenuBar.tsx
+++ src/components/organisms/MenuBar.tsx
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import CartIcon from '../atoms/CartIcon';
 import ProfileIcon from '../atoms/ProfileIcon';
+import Text from '../atoms/Text';
 import styled from 'styled-components';
 
 const IconWrapper = styled.div`
@@ -12,6 +13,9 @@ export default function MenuBar() {
   return (
     <AppBar position="static">
       <Toolbar>
+        <Text>
+          Cart
+        </Text>
         <IconWrapper>
           <CartIcon />
           <ProfileIcon />
