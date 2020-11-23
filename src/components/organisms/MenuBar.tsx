7704009bb2d8e804aa28aba57256ed8c855d5c01
dak2 <dawt2h@gmail.com>
2020-11-23 14:08:26 +0900

    ヘッダーバーにstyled components適用

--- src/components/organisms/MenuBar.tsx
+++ src/components/organisms/MenuBar.tsx
@@ -3,13 +3,19 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import CartIcon from '../atoms/CartIcon';
 import ProfileIcon from '../atoms/ProfileIcon';
+import styled from 'styled-components';
 
+const IconWrapper = styled.div`
+  margin-left: auto;
+`
 export default function MenuBar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <CartIcon />
-        <ProfileIcon />
+        <IconWrapper>
+          <CartIcon />
+          <ProfileIcon />
+        </IconWrapper>
       </Toolbar>
     </AppBar>
   );
